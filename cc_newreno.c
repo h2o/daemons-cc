5b89744b8f822e2eca7403ceadb013514ffeda4d
Kazuho Oku <kazuhooku@gmail.com>
2018-06-16 11:53:40 +0900

    use standard types

--- cc_newreno.c
+++ cc_newreno.c
@@ -68,8 +68,8 @@ newreno_ack_received(struct cc_var *ccv, uint16_t type)
 {
 	if (type == CC_ACK && !IN_RECOVERY(CCV(ccv, t_flags)) &&
 	    (ccv->flags & CCF_CWND_LIMITED)) {
-		u_int cw = CCV(ccv, snd_cwnd);
-		u_int incr = CCV(ccv, t_maxseg);
+		unsigned cw = CCV(ccv, snd_cwnd);
+		unsigned incr = CCV(ccv, t_maxseg);
 
 		/*
 		 * Regular in-order ACK, open the congestion window.
@@ -164,7 +164,7 @@ newreno_after_idle(struct cc_var *ccv)
 static void
 newreno_cong_signal(struct cc_var *ccv, uint32_t type)
 {
-	u_int win;
+	unsigned win;
 
 	/* Catch algos which mistakenly leak private signal types. */
 	KASSERT((type & CC_SIGPRIVMASK) == 0,
